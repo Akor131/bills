4132972457c288c454813d6b28d902ee9e84c819
Akor Emmanuel <emmanuelakor131@gmail.com>
2024-10-09 13:45:31 +0100

    dashboard UI

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,6 +2,9 @@ export default function Dashboard () {
     return (
         <main>
             <p>User dashboard</p>
+            <p>propose a UI design</p>
+            <p>Build the UI</p>
+            <p>propose possible changes</p>
         </main>
     )
 }
